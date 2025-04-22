6789113bfabdcbf43088001fe00f0ffed781d492
harunandes21 <102485008+harunandes21@users.noreply.github.com>
2025-04-21 17:33:17 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/api/contact', async (req, res) => {
+    const { name, email, pnumber, message, moveDate } = req.body;
+
+    if (!name || !email || !pnumber || !message) {
+        return res.status(400).json({ error: 'All fields are required.' });
+    }
+
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'tucsonletsgetmoving@gmail.com',
+                pass: 'rsklyxddvscdiwol'
+            }
+        });
+
+        const mailOptions = {
+            from: email,
+            to: 'tucsonletsgetmoving@gmail.com',
+            subject: `Let's Get Moving Contact Form - ${pnumber}`,
+            text: `Name: ${name}\nEmail: ${email}\nPhone: ${pnumber}\n\nPreferred Move Date: ${moveDate} \nMessage:\n${message}`
+        };
+
+        await transporter.sendMail(mailOptions);
+        console.log('✅ Email sent');
+        res.json({ success: true });
+    } catch (error) {
+        console.error('❌ Mail Error:', error);
+        res.status(500).json({ error: 'Failed to send message' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
