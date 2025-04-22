4bb4bf9274b54ec4811afa9017d61854d9c56945
harunandes21 <102485008+harunandes21@users.noreply.github.com>
2025-04-21 23:25:17 -0700

    updates

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
+const compression = require('compression');
+app.use(compression());
+
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
